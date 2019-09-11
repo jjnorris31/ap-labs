72814b3c9f0efb51d3120e54c8d040786789c34f
jjnorris31 <juan.gonbasilio@gmail.com>
2019-09-10 20:15:48 -0500

    go exercises

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,26 @@
 package main
 
-func main() {
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	myMap := make(map[string]int)
+	
+	for j := 0; j < len(words); j++ {
+		_, ok := myMap[words[j]]
+		if ok {
+			myMap[words[j]]++
+		} else {
+			myMap[words[j]] = 1
+		}
+	}
+	return myMap
 }
+
+func main() {
+	wc.Test(WordCount)
+}
+
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,21 @@
 package main
 
-func main() {
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	dy_slice := make([][]uint8, dy)
+
+		for y := range dy_slice {
+			dy_slice[y] = make([]uint8, dx)
+			for x := range dy_slice[y] {
+				v := ((x + y) / 2)
+				dy_slice[y][x] = uint8(v)
+			}
+		}
+		return dy_slice
 }
+
+func main() {
+	pic.Show(Pic)
+}
+
