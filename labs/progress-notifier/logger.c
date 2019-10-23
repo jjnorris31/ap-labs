a10edf0c3cce3afcb78fdacaf0e8b36d70e9b93d
jjnorris31 <juan.gonbasilio@gmail.com>
2019-10-22 23:22:39 -0500

    progress notifier finished

--- labs/progress-notifier/logger.c
+++ labs/progress-notifier/logger.c
@@ -1,23 +1,96 @@
 #include <stdio.h>
-#include "logger.h"
+#include <stdarg.h>
 
-int initLogger(char *logType) {
-    printf("Initializing Logger on: %s\n", logType);
-    return 0;
+#define RESET		0
+#define BRIGHT 		1
+#define HIDDEN		8
+
+#define BLACK 		0
+#define RED		    1
+#define L_RED       61
+#define L_YELLOW    63
+#define BLUE		4
+#define	WHITE		9
+
+/* infof prototype */
+int infof(const char *format, ...);
+/* warnf prototype */
+int warnf(const char *format, ...);
+/* errorf prototype */
+int errorf(const char *format, ...);
+/* panicf prototype */
+int panicf(const char *format, ...);
+
+void textcolor(int attr, int fg, int bg)
+{	char command[13];
+    sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg + 30, bg + 40);
+    printf("%s", command);
 }
 
-int infof(const char *format, ...) {
-    return 0;
+int infof (const char *format, ...){
+    // variable-length argument list
+    va_list arg;
+    int done;
+
+    textcolor(BRIGHT, WHITE, BLUE);
+    printf("INFO:");
+    textcolor(RESET, WHITE, HIDDEN);
+    printf(" ");
+
+    va_start(arg, format); // initializing arguments to store all values after format
+    done = vprintf(format, arg);
+    va_end(arg); // cleans up the list
+    printf("\n");
+    return done;
 }
 
-int warnf(const char *format, ...) {
-    return 0;
+int warnf (const char *format, ...){
+    // variable-length argument list
+    va_list arg;
+    int done;
+
+    textcolor(BRIGHT, BLACK, L_YELLOW);
+    printf("WARNING:");
+    textcolor(RESET, WHITE, HIDDEN);
+    printf(" ");
+
+    va_start(arg, format); // initializing arguments to store all values after format
+    done = vprintf(format, arg);
+    va_end(arg); // cleans up the list
+    printf("\n");
+    return done;
 }
 
-int errorf(const char *format, ...) {
-    return 0;
-}
-int panicf(const char *format, ...) {
-    return 0;
+int errorf (const char *format, ...){
+    // variable-length argument list
+    va_list arg;
+    int done;
+
+    textcolor(BRIGHT, WHITE, RED);
+    printf("ERROR:");
+    textcolor(RESET, WHITE, HIDDEN);
+    printf(" ");
+
+    va_start(arg, format); // initializing arguments to store all values after format
+    done = vprintf(format, arg);
+    va_end(arg); // cleans up the list
+    printf("\n");
+    return done;
 }
 
+int panicf (const char *format, ...){
+    // variable-length argument list
+    va_list arg;
+    int done;
+
+    textcolor(BRIGHT, WHITE, L_RED);
+    printf("PANIC:");
+    textcolor(RESET, WHITE, HIDDEN);
+    printf(" ");
+
+    va_start(arg, format); // initializing arguments to store all values after format
+    done = vprintf(format, arg);
+    va_end(arg); // cleans up the list
+    printf("\n");
+    return done;
+}
