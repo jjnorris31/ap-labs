ccc7d7bfbca5a74064fc7ddaa4972f1bb25a93c8
jjnorris31 <jjnorris31@gmail.com>
2019-11-05 23:19:01 -0600

    Revert "Revert "logger finished""
    This reverts commit 5dda2f41211b049dec7c1329ac378dba033e7437.

--- labs/logger/testLogger.c
+++ labs/logger/testLogger.c
@@ -1,5 +1,15 @@
+#include "logger.c"
 
+int infof(const char *format, ...);
+int warnf(const char *format, ...);
+int errorf(const char *format, ...);
+int panicf(const char *format, ...);
 
-int main() {
+int main()
+{
+    infof("Outputs an informational message to the console.\n");
+    warnf("Write a warning message in the console.\n");
+    errorf("An important message representing an unresolved failure.\n");
+    panicf("Errors that we aren’t prepared to handle gracefully.\n");
     return 0;
 }
