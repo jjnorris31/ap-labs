02fce4943196b1fecc11ef592d7fb67caf96af45
jjnorris31 <jjnorris31@gmail.com>
2019-11-05 18:52:38 -0600

    Revert "str-lb ready"
    This reverts commit 91983f87566117b761a37ea45b5fde35e0b288b2.

--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,42 +1,13 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+
 
 int mystrlen(char *str){
-    int n;
-    for (n = 0; *str != '\0'; str++) {
-        n++;
-    }
-
-    return n;
+    return 0;
 }
 
 char *mystradd(char *origin, char *addition){
-    char* result = malloc(sizeof(char)*mystrlen(origin) + mystrlen(addition));
-    strcpy(result, origin);
-    strcpy(result + mystrlen(origin), addition);
-    return result;
+    return 0;
 }
 
 int mystrfind(char *origin, char *substr){
-    int n;
-    int o;
-    int tempn;
-    int res = 0;
-    for (n = 0; origin[n] != '\0'; n++) {
-        tempn = n;
-        for (o = 0; substr[o] != '\0'; o++){
-            if (origin[tempn] == substr[o]) {
-                tempn++;
-                res = 1;
-            } else {
-                res = 0;
-                break;
-            }
-        }
-        if (res == 1) {
-            return 1;
-        }
-    }
-    return res;
+    return 0;
 }
