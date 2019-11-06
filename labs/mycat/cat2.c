68e1a2742fff905e06f6ab6a988e1bd8e3056453
jjnorris31 <jjnorris31@gmail.com>
2019-11-05 18:55:09 -0600

    Revert "mycat2 finished"
    This reverts commit 1aa1baa7215ab6ad93c61085be24738b0a27d57f.

--- labs/mycat/cat2.c
+++ labs/mycat/cat2.c
@@ -1,44 +1,33 @@
 #include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
 
-int filecopy(int fd)
+/* filecopy:  copy file ifp to file ofp */
+void filecopy(FILE *ifp, FILE *ofp)
 {
-    char buf[8192];
-    ssize_t number_r;
+    int c;
 
-    while ((number_r = read(fd, buf, sizeof buf)) > 0) {
-        ssize_t total_w = 0;
-        while (total_w < number_r) {
-            ssize_t number_w = write(STDOUT_FILENO, buf + total_w, number_r - total_w);
-            if (number_w < 1) {
-                return -1;
-            }
-            total_w += number_w;
-        }
-    }
+    while ((c = getc(ifp)) != EOF)
+        putc(c, ofp);
 
-    return number_r == 0 ? 0 : -1;
 }
 
 /* cat:  concatenate files, version 2 */
 int main(int argc, char *argv[])
 {
-    int fd, success;
+    FILE *fp;
+    void filecopy(FILE *, FILE *);
     char *prog = argv[0];   /* program name for errors */
 
     if (argc == 1)  /* no args; copy standard input */
-        fprintf(stderr, "%s: no args\n", prog);
+        filecopy(stdin, stdout);
     else
         while (--argc > 0)
-            if ((fd = open(*++argv, O_RDONLY)) == -1) {
-                fprintf(stderr, "%s: can′t open %s\n", prog, *argv);
+            if ((fp = fopen(*++argv, "r")) == NULL) {
+                fprintf(stderr, "%s: can′t open %s\n",
+			prog, *argv);
                 return 1;
             } else {
-                filecopy(fd);
-                if (close(fd) != 0) {
-                    return -1;
-                }
+                filecopy(fp, stdout);
+                fclose(fp);
             }
 
     if (ferror(stdout)) {
